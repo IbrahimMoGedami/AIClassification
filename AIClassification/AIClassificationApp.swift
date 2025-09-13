2daf70279690ebf1e1c784cc128a08bdb02ef69e
Ibrahim Mohammed Gedami <hemagedamy@gmail.com>
2025-09-13 15:09:25 +0400

    Initial Commit

---
+++ AIClassification/AIClassificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIClassificationApp.swift
+//  AIClassification
+//
+//  Created by Ibrahim Mo Gedami on 13/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct AIClassificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIClassificationTests/AIClassificationTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIClassificationTests.swift
+//  AIClassificationTests
+//
+//  Created by Ibrahim Mo Gedami on 13/09/2025.
+//
+
+import Testing
+@testable import AIClassification
+
+struct AIClassificationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
